0f1f98e606ff7bdc8019d7fa1d3031d63c008e71
Dio Restu Saputra <79250076+diorestu@users.noreply.github.com>
2021-02-20 13:10:20 +0800

    coba commit

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap core JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js" integrity="sha512-WNLxfP/8cVYL9sj8Jnp6et0BkubLP31jhTG9vhL/F5uEZmg5wEzKoXp1kJslzPQWwPT1eyMiSxlKCgzHLOTOTQ==" crossorigin="anonymous"></script>
+<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+<script src="/script/navbar-scroll.js"></script>
